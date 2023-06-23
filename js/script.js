214594879cf224bcd47672f0db80c1f368cdf5fd
VC_Sophea <111986860+imPhea@users.noreply.github.com>
2023-06-23 17:31:47 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// navbar
+const body = document.querySelector("body"),
+  nav = document.querySelector("nav"),
+  sidebarOpen = document.querySelector(".sidebarOpen"),
+  siderbarClose = document.querySelector(".siderbarClose");
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode") {
+  body.classList.add("dark");
+}
+
+//   js code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+});
+
+body.addEventListener("click", e => {
+  let clickedElm = e.target;
+
+  if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
+    nav.classList.remove("active");
+  }
+});
